aad1bc1f5b0b3cbbff9b89fbb1de9aab58f1f2b7
levi9111 <captainlevi9111@gmail.com>
2025-06-13 15:45:52 +0600

    navbar initiated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image';
+import logo from '../../public/logos/logo.svg';
+
+const links = [
+  { title: 'Home', path: '' },
+  { title: 'Portfolio', path: '' },
+  { title: 'Services', path: '' },
+  { title: 'Review', path: '' },
+  { title: 'FAQ', path: '' },
+];
 
 const Navbar = () => {
   return (
-    <nav>Navbar</nav>
-  )
-}
+    <nav className='bg-quartz'>
+      <div className=''>
+        <Image src={logo} alt='UIX Design Lab Logo' width={56} height={53} />
+        <p className=''>Design lab</p>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
