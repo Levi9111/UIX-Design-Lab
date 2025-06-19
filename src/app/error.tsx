43fb28b1d449bc0ff3507275f37b775e7d686b5e
levi9111 <captainlevi9111@gmail.com>
2025-06-19 16:03:40 +0600

    loading page added.

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-// app/special/error.tsx
 import { useEffect } from 'react';
 
 export default function ErrorPage({
@@ -16,7 +15,7 @@ export default function ErrorPage({
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen text-center'>
-      <h1 className='text-4xl font-bold'>Something went wrong</h1>
+      <h1 className='text-4xl font-bold text-white'>Something went wrong</h1>
       <button
         onClick={() => reset()}
         className='mt-6 px-4 py-2 bg-white text-black rounded'
