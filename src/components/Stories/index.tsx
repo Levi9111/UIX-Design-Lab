a03ffb72fb7fa9ead3e51cb304f893d0f2b05bcb
levi9111 <captainlevi9111@gmail.com>
2025-06-20 10:54:36 +0600

    carousel bug fix -1

--- src/components/Stories/index.tsx
+++ src/components/Stories/index.tsx
@@ -10,16 +10,16 @@ const Stories = () => {
 
   return (
     <section id='review' className='md:pt-[180px] pt-16'>
-      <div className='uix-center relative '>
+      <div className='uix-center relative overflow-hidden'>
         <PlanetText
           btnText='Our clients'
           title='Our Success Stories'
           subtitle='Real transformations from real businesses. See how our solutions drive exceptional results.'
         />
+        {device === 'desktop' && <DesktopStories />}
+        {device === 'tablet' && <TabletStories />}
+        {device === 'mobile' && <MobileStories />}
       </div>
-      {device === 'desktop' && <DesktopStories />}
-      {device === 'tablet' && <TabletStories />}
-      {device === 'mobile' && <MobileStories />}
     </section>
   );
 };
